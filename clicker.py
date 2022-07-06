3fc249828aeec4013b5e0ea0483cffe5e93adc92
Markella's <105635627+Markellas@users.noreply.github.com>
2022-07-06 13:43:52 +0300

    Update clicker.py

--- clicker.py
+++ clicker.py
@@ -7,9 +7,11 @@ print("Welcome to the InstantClick!")
 while True:
     clcks_amount = input("Enter the amount of clicks: ")
     clcks_amount = int(clcks_amount)
+    interval_var = input("Enter the interval between clicks (in secs): ")
+    interval_var = int(interval_var)
     timetospend = input("Enter the wait time before the click: ")
     timetospend = int(timetospend)
 
     time.sleep(timetospend)
 
-    root.click(clicks=clcks_amount)
+    root.click(clicks=clcks_amount, interval=interval_var
