aeb8b4d6db02f43d689e643abfbf5356262d15d7
Markella's <105635627+Markellas@users.noreply.github.com>
2022-07-24 17:51:39 +0300

    Update clicker.py

--- clicker.py
+++ clicker.py
@@ -5,17 +5,22 @@ from time import sleep
 print("Welcome to the InstantClicker by Markella's!")
 
 while True:
+    print("")
     clcks_amount = input("Enter the amount of clicks: ")
     clcks_amount = int(clcks_amount)
+    print("")
     interval_var = input("Enter the interval between clicks (in secs): ")
     interval_var = int(interval_var)
+    print("")
     timetospend = input("Enter the wait time before the click: ")
     timetospend = int(timetospend)
+    print("")
 
     print(f"InstantClicker is waiting for {timetospend} seconds to you to place the cursor...")
+    print("")
 
     time.sleep(timetospend)
 
     root.click(clicks=clcks_amount, interval=interval_var)
 
-    print("Clicks were succsessful! Continue using InstantClicker if want")
+    print("Clicks were succsessful! Continue using InstantClicker if want"))
